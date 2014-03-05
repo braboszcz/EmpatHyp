d96f885ac5a992cf153536a1fe8a48b9afd369be
Claire <claire@guakamole.org>
2014-03-05 13:22:04 +0100

    new generate trials that are now counterbalanced

--- Expe/Python/generateTrials.py
+++ Expe/Python/generateTrials.py
@@ -7,118 +7,165 @@ import random
 import os
 import csv
 import numpy
+import ipdb
 
 GO = 1
-NOGO = 2
+NEG= 2
 ODDBALL = 3
 PAUSE = 4
 NULL = 5
+NEUT=6
 
 #MAXGOINROW = 2
 
 def lefthand(stim):
 	return True if stim[0] in  ['L'] else False
 
-def righthand(stim):
-	return True if stim[0] in  ['R'] else False
+#def righthand(stim):
+#	return True if stim[0] in  ['R'] else False
 
 def oddball(stim):
     return True if stim[0] in ['o'] else False
 
+def neg(stim):
+    return True if stim[4] in ['E'] and stim[0] in ['R'] else False
+
+def neut(stim):
+    return True if stim[4] in ['N'] and stim[0] in ['R'] else False
+
 is_go = lefthand
-is_odd = oddball
+
 
 def genTrialList():
-	""" 
-	generate trial list
-	if expe = fmri use fmri jittered ISI list else jitter between 1 and 2 sec	
-
+    """ 
+    generate trial list for empathyp
+    equal number of 
 	"""
-	
-	writeTrials = csv.writer(open('mytrialList.csv','wb'), delimiter = ',', quotechar = '"')
-	header = ['Stim', 'Condition', 'ITI']
-	writeTrials.writerow(header)
-	
-	#------------------------------------------------------------------------
-	# define numbers of each type of trials
-	#------------------------------------------------------------------------
-	nNoGo = 30*2
-	nGo = 12 #(  20% de nNoGo. allows for 8 pictures of each conditions)
-	nOdd = 5	
+    #------------------------------------------------------------------------
+    # read stim file name in folder
+    #------------------------------------------------------------------------
+    for root, dirs, files in os.walk('Stimuli'): stimList = files
+    random.shuffle(stimList)
+    #------------------------------------------------------------------------   # categorize stim into go and no go list
+    #------------------------------------------------------------------------
+    goStim = [stim for stim in stimList if is_go(stim)]
+    neutStim = [stim for stim in stimList if neg(stim)]
+    negStim = [stim for stim in stimList if neut(stim)]
+    oddStim = [stim for stim in stimList if oddball(stim)]
+    
+    random.shuffle(goStim)
+    random.shuffle(neutStim)
+    random.shuffle(negStim)
+    random.shuffle(oddStim)
 
-	#------------------------------------------------------------------------
-	# read stim file name in folder
-	#------------------------------------------------------------------------
-	for root, dirs, files in os.walk('Stimuli'):
-		stimList = files
-	random.shuffle(stimList)
-	
-	#------------------------------------------------------------------------   # categorize stim into go and no go list
-	#------------------------------------------------------------------------
-	goStim = [stim for stim in stimList if is_go(stim)]
-	nogoStim = [stim for stim in stimList if not is_go(stim)]
-	random.shuffle(goStim)
-	random.shuffle(nogoStim)	
-	oddballStim = [stim for stim in stimList if is_odd(stim)]
+    #------------------------------------------------------------------------	# read jitter list
+    #------------------------------------------------------------------------
+    with open('fMRI_jitter.txt') as jitter:
+        ISI = jitter.read().splitlines()	
 
-	#------------------------------------------------------------------------	# read jitter list
-	#------------------------------------------------------------------------
-	with open('fMRI_jitter.txt') as jitter:
-		ISI = jitter.read().splitlines()	
+    #------------------------------------------------------------------------
+    # define numbers of each type of trials
+    #------------------------------------------------------------------------
+    nNeg = len(negStim)/4
+    nNeut= len(neutStim)/4
+    nGo = len(goStim)/4 #(  20% de nNoGo. allows for 8 pictures of each conditions)
+    nOdd = len(oddStim)/4	
 
-	#------------------------------------------------------------------------	# create list of desired amount of Go and No Go Trials   
-	#-----------------------------------------------------------------------
+    indstart = 0
+    indstop = nNeg
+    Go_indstart= 0
+    Go_indstop = nGo
+    Odd_indstart = 0
+    Odd_indstop = nOdd     
 
-	# mix Go and No Go
-	
-	trials = [NOGO] * nNoGo
-	trials += [GO] * nGo
-	trials += [ODDBALL] * nOdd
-	#trials += [NULL]*nNull
-	random.shuffle(trials)
-	
-	# insert regular null event for resting control condition
-	for i in range (1, len(trials)+len(trials)/6,6):
-		trials.insert(i, NULL)	
-		print i	
+    for run in range(4):
 
-	# then insert PAUSE (hypnosis reinforcement) 
-	for i in range(1,len(trials), len(trials)/2):
-		if i != 1:
-			trials.insert(i,PAUSE) 
-	#------------------------------------------------------------------------
-  	# create list of trials
-	#------------------------------------------------------------------------
-	
-	indnoGo = 0
-	indGo = 0
-	indOdd = 0
-	indNull = 0
-	trial = [] 
-	
-	for i in range(len(trials)):
-		if trials[i] == 3:
-		    stim   = oddballStim[indOdd]
-		    trial  = [stim ,'oddball', str(ISI[i])]
-		    indOdd +=1
-		elif trials[i] == 5:
-			trial = ['blank', 'blank', 5]
+        writeTrials = csv.writer(open('run' + str(run) + '.csv','wb'), delimiter = ',', quotechar = '"')
+        header = ['Stim', 'Condition', 'ITI']
+        writeTrials.writerow(header)
+        
+        #------------------------------------------------------------------------	
+        # create list of desired amount of each type of Trials   
+        #-----------------------------------------------------------------------
 
-		elif trials[i] == 4:
-			trial = ['pause', 'pause']
+        # mix Go and No Go
+        
+        trials = [GO] * nGo
+        trials += [NEG] * nNeg
+        trials += [NEUT]* nNeut
+        trials += [ODDBALL] * nOdd
+        #trials += [NULL]*nNull
+        random.shuffle(trials)
+        
+        #---------------------------------
+        # Create stimuli list for this run
+        #---------------------------------
+       
+        Neg_run = negStim[indstart:indstop]
+        Neut_run = neutStim[indstart:indstop]
+        indstart += nNeg
+        indstop += nNeg
+        print indstart
+        print indstop
 
-		elif trials[i] == 2: 	
-			stim = nogoStim[indnoGo]
-			trial = [stim,'no-go',str(ISI[i])]
-			indnoGo += 1
-		#	print indnoGo
-		elif trials[i] == 1:	
-			stim = goStim[indGo]
-			
-			trial = [stim,'go',str(ISI[i])]
-			indGo += 1
-		#	print indGo
-		writeTrials.writerow(trial)
+        Go_run = goStim[Go_indstart:Go_indstop]
+        Go_indstart += nGo
+        Go_indstop += nGo
+        
+        
+        
+        Odd_run = oddStim[Odd_indstart:Odd_indstop]
+        Odd_indstart += nOdd
+        Odd_indstop += nOdd
+        
+        print Neg_run, Neut_run, Go_run, Odd_run
+        # insert regular null event for resting control condition
+        for i in range (1, len(trials)+len(trials)/6,6):
+            trials.insert(i, NULL)	
+            
+
+        # then insert PAUSE (hypnosis reinforcement) 
+       # for i in range(1,len(trials), len(trials)/2):
+        #    if i != 1:
+        #      trials.insert(i,PAUSE) 
+        
+        #------------------------------------------------------------------------
+        # create list of trials
+        #------------------------------------------------------------------------
+        
+        indGo = 0
+        indNeg = 0
+        indNeut= 0
+        indOdd = 0
+        indNull = 0
+        trial = [] 
+        
+        for i in range(len(trials)):
+            if trials[i] == 3:
+                stim   = Odd_run[indOdd]
+                trial  = [stim ,'oddball', str(ISI[i])]
+                indOdd +=1
+            elif trials[i] == 5:
+                trial = ['blank', 'blank', 5]
+
+            elif trials[i] == 4:
+                trial = ['pause', 'pause']
+
+            elif trials[i] == 2: 	
+                stim = Neg_run[indNeg]
+                trial = [stim,'neg',str(ISI[i])]
+                indNeg += 1
+            elif trials[i] == 6: 	
+                stim = Neut_run[indNeut]
+                trial = [stim,'neut',str(ISI[i])]
+                indNeut += 1
+                
+            elif trials[i] == 1:	
+                stim = Go_run[indGo]
+                trial = [stim,'go',str(ISI[i])]
+                indGo += 1
+            writeTrials.writerow(trial)
+
 	
 
 
